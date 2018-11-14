85342438f068007ce794295caa386c2e1291d66f
Joakim Anell <jocke.anell@gmail.com>
2018-11-14 19:05:32 +0100

    expense query fixes

--- src/se/an3ll/ktor/koin/app/persistence/repo/ExpenseRepository.kt
+++ src/se/an3ll/ktor/koin/app/persistence/repo/ExpenseRepository.kt
@@ -3,8 +3,11 @@ package se.an3ll.ktor.koin.app.persistence.repo
 import com.mongodb.MongoClient
 import org.bson.types.ObjectId
 import org.litote.kmongo.Id
+import org.litote.kmongo.div
+import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 import se.an3ll.ktor.koin.app.persistence.model.Expense
+import se.an3ll.ktor.koin.app.persistence.model.User
 
 class ExpenseRepository(private val client: MongoClient) : Repository<Expense> {
   override fun insert(objectToInsert: Expense) {
@@ -12,8 +15,9 @@ class ExpenseRepository(private val client: MongoClient) : Repository<Expense> {
   }
 
   override fun getById(id: Id<Expense>): Expense? {
-    val user = context(client).findOne("{'expenses.amount' : 78.99}")
-    return user?.expenses?.find { it._id == id }
+//    val user = context(client).findOne("{'expenses.amount' : 78.99}")
+    val user = context(client).findOne(User::expenses / Expense::_id eq ObjectId(id.toString()))
+    return user?.expenses?.find { it._id.toString() == id.toString() }
   }
 
   override fun update(objectToUpdate: Expense) {
